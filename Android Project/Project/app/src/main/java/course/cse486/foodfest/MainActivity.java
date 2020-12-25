df1b1375add08647ecb69e496fab29a51a25d124
Emran Hossain <emran.hossain1@northsouth.edu>
2020-12-25 21:12:31 +0600

    initial commit

---
+++ Android Project/Project/app/src/main/java/course/cse486/foodfest/MainActivity.java
@@ -0,0 +1,14 @@
+package course.cse486.foodfest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
